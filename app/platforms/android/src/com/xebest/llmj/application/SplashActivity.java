0825f24be02cbc9d7fbe48fb7c17268ae06b1a12
sherry <lzjfsk@sina.com>
2015-10-21 14:38:28 +0800

    mre

--- app/platforms/android/src/com/xebest/llmj/application/SplashActivity.java
+++ app/platforms/android/src/com/xebest/llmj/application/SplashActivity.java
@@ -3,6 +3,9 @@ package com.xebest.llmj.application;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.xebest.llmj.MainActivity;
+import com.xebest.llmj.R;
+
 /**
  * 闪屏页
  * Created by kaisun on 15/10/21.
@@ -12,6 +15,30 @@ public class SplashActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Application.getInstance().addActivity(this);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(3000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                MainActivity.actionView(SplashActivity.this, 0);
+//                SharedPreferences sp = getSharedPreferences(ApiUtils.IS_FIRST_IN, 0);
+//                Boolean firstTime = sp.getBoolean("first", true);
+//                if (firstTime) {
+//                    // 进入引导页
+//                    GuideActivity.actionView(SplashActivity.this);
+//                } else {
+//                    MainActivity.actionView(SplashActivity.this, 0, 0);
+//                }
+            }
+        }).start();
+
     }
 
 }
