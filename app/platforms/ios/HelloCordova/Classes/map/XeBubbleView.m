ed5bfc407330de08a96d7a147d214db7ae644e14
ywen <yaliyingwy@gmail.com>
2015-11-03 11:20:34 +0800

    fix bug

--- app/platforms/ios/HelloCordova/Classes/map/XeBubbleView.m
+++ app/platforms/ios/HelloCordova/Classes/map/XeBubbleView.m
@@ -9,6 +9,14 @@
 #import "XeBubbleView.h"
 #import "Global.h"
 
+#import "PersonalGoodsAuthViewController.h"
+#import "CompanyGoodsAuthViewController.h"
+#import "PersonalCarAuthViewController.h"
+#import "CompanyCarAuthViewController.h"
+#import "PersonalWarehouseAuthViewController.h"
+#import "CompanyWarehouseAuthViewController.h"
+#import "AuthViewController.h"
+
 @implementation XeBubbleView
 
 - (instancetype)initWithFrame:(CGRect)frame
@@ -32,6 +40,113 @@
     
 }
 
+-(void)checkAuth:(AuthType)type cb:(AuthCb)cb {
+    NSDictionary *user = [Global getUser];
+    NSInteger authStatus;
+    NSInteger certification = [[user objectForKey:@"certification"] integerValue];
+    
+    if (certification == 0) {
+        [YwenAlert alert:@"尚未通过任何角色的认证！" vc:[Global sharedInstance].mapVC confirmStr:@"去认证" confirmCb:^{
+            AuthViewController *authVC = [AuthViewController new];
+            [[Global sharedInstance].mapVC.navigationController pushViewController:authVC animated:YES];
+        }];
+    }
+    else
+    {
+        switch (type) {
+            case CarAuth:
+            {
+                authStatus = [[user objectForKey:@"carStatus"] integerValue];
+                if (authStatus == 0) {
+                    [YwenAlert alert:@"尚未通过车主认证！" vc:[Global sharedInstance].mapVC confirmStr:@"去认证" confirmCb:^{
+                        UIViewController *authVC;
+                        if (certification == 1) {
+                            authVC = [PersonalCarAuthViewController new];
+                        }
+                        else
+                        {
+                            authVC = [CompanyCarAuthViewController new];
+                        }
+                        
+                        [[Global sharedInstance].mapVC.navigationController pushViewController:authVC animated:YES];
+                    } cancelStr:@"取消" cancelCb:nil];
+                }
+                else if (authStatus == 2)
+                {
+                    [[Global sharedInstance] showErr:@"车主正在审核中，请审核通过后再试"];
+                }
+                else
+                {
+                    cb();
+                }
+                
+            }
+                break;
+                
+            case GoodsAuth:
+            {
+                authStatus = [[user objectForKey:@"goodsStatus"] integerValue];
+                if (authStatus == 0) {
+                    [YwenAlert alert:@"尚未通过货主认证！" vc:[Global sharedInstance].mapVC confirmStr:@"去认证" confirmCb:^{
+                        UIViewController *authVC;
+                        if (certification == 1) {
+                            authVC = [PersonalGoodsAuthViewController new];
+                        }
+                        else
+                        {
+                            authVC = [CompanyGoodsAuthViewController new];
+                        }
+                        
+                        [[Global sharedInstance].mapVC.navigationController pushViewController:authVC animated:YES];
+                    } cancelStr:@"取消" cancelCb:nil];
+                }
+                else if (authStatus == 2)
+                {
+                    [[Global sharedInstance] showErr:@"货主正在审核中，请审核通过后再试"];
+                }
+                else
+                {
+                    cb();
+                }
+                
+            }
+                break;
+
+            case WarehouseAuth:
+            {
+                authStatus = [[user objectForKey:@"carStatus"] integerValue];
+                if (authStatus == 0) {
+                    [YwenAlert alert:@"尚未通过仓库主认证！" vc:[Global sharedInstance].mapVC confirmStr:@"去认证" confirmCb:^{
+                        UIViewController *authVC;
+                        if (certification == 1) {
+                            authVC = [PersonalWarehouseAuthViewController new];
+                        }
+                        else
+                        {
+                            authVC = [CompanyWarehouseAuthViewController new];
+                        }
+                        
+                        [[Global sharedInstance].mapVC.navigationController pushViewController:authVC animated:YES];
+                    } cancelStr:@"取消" cancelCb:nil];
+                }
+                else if (authStatus == 2)
+                {
+                    [[Global sharedInstance] showErr:@"仓库主正在审核中，请审核通过后再试"];
+                }
+                else
+                {
+                    cb();
+                }
+                
+            }
+                break;
+                
+            default:
+                break;
+        }
+    }
+    
+}
 
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
